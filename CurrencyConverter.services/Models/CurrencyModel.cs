856fab13754bb351923debf70f8880d45afc59a1
KingsleyOghonyon <kingsleyigho35@gmail.com>
2024-09-11 20:11:40 -0500

    initial commit
    initial commit

---
+++ CurrencyConverter.services/Models/CurrencyModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CurrencyConverter.services.models
+{
+    // Root myDeserializedClass = JsonConvert.DeserializeObject<Root>(myJsonResponse);
+    public class Rates
+    {
+        public double EUR { get; set; }
+        public double GBP { get; set; }
+    }
+
+    public class Root
+    {
+        public string @base { get; set; }
+        public string date { get; set; }
+        public Rates rates { get; set; }
+        public bool success { get; set; }
+        public int timestamp { get; set; }
+    }
+   
+
+
+  
+
+}
