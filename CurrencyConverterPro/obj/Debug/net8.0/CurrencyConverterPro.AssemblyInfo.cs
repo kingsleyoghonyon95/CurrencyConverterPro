8479580735040743b6bda63bb9d2c78b07d3accf
kingsleyoghonyon95 <kingsleyigho35@gmail.com>
2024-09-17 18:09:49 -0500

    set up services

--- CurrencyConverterPro/obj/Debug/net8.0/CurrencyConverterPro.AssemblyInfo.cs
+++ CurrencyConverterPro/obj/Debug/net8.0/CurrencyConverterPro.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CurrencyConverterPro")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+856fab13754bb351923debf70f8880d45afc59a1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+201c8e9ed68fa2233153d1bfac02d022c1fe475b")]
 [assembly: System.Reflection.AssemblyProductAttribute("CurrencyConverterPro")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CurrencyConverterPro")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
